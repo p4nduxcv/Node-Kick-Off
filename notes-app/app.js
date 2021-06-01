7da49aade9d2e2689c906c08ff35d0e848fcceec
pandula <pandula95@gmail.com>
2021-06-01 21:40:58 +0530

    user input with Yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,44 @@
-const add = require("./utils.js");
-const getNotes = require("./notes.js");
 const validator = require("validator");
 const chalk = require("chalk");
+const yargs = require("yargs");
 
-//const fs = require("fs");
+const getNotes = require("./notes.js");
 
-//fs.writeFileSync("notes.txt", "pakaya node walin haduwe");
-// fs.appendFileSync("notes.txt", "data to append");
-// console.log('The "data to append" was appended to file!');
-console.log(`sum`, add(5, 2));
-console.log(`notes`, getNotes());
-console.log(validator.isEmail("pandulka95@gmail.com"));
-console.log(chalk.bgMagenta("Success gg"));
+//command create
+yargs.command({
+  command: "add",
+  describe: "Add a new Note",
+  handler: function () {
+    console.log(chalk.bgBlue("adding a new note"));
+  },
+});
+
+//command remove
+yargs.command({
+  command: "remove",
+  describe: "Remove a new Note",
+  handler: function () {
+    console.log(chalk.bgRed("Removing a new note"));
+  },
+});
+
+//command read
+yargs.command({
+  command: "read",
+  describe: "Read a new Note",
+  handler: function () {
+    console.log(chalk.bgGreen("Reading a new note"));
+  },
+});
+
+//command list
+yargs.command({
+  command: "list",
+  describe: "List a new Note",
+  handler: function () {
+    console.log(chalk.bgGray("Listing a new note"));
+  },
+});
+
+// add,remove, read, list
+console.log(yargs.argv);
