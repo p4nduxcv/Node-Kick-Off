8ead6b609c91138b28ce810ace03429242363559
pandula <pandula95@gmail.com>
2021-06-13 12:55:33 +0530

    Make HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,10 @@
-console.log("starting");
+const request = require("request");
 
-setTimeout(() => {
-  console.log("5 Sec");
-}, 5000);
+const url =
+  "http://api.weatherstack.com/current?access_key=050816e4518d6edd2f351d54f1e7345b&query=New%20York";
 
-console.log("stopping");
+request(url, (error, response) => {
+  const _weatherdata = JSON.parse(response.body);
+
+  console.log(_weatherdata.current);
+});
