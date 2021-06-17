65e3251cabf14224ee71336ba7a4b8fb62aa9719
pandula <pandula95@gmail.com>
2021-06-18 00:01:39 +0530

    refactor: Property Shorthhand

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -1,10 +1,18 @@
 //destructuring
 
+const { type } = require("os");
+
 const products = {
   name: "pakaya",
   price: 45,
 };
 
-const { name: productName, price } = products;
-console.log(productName);
-console.log(price);
+// const { name: productName, price } = products;
+// console.log(productName);
+// console.log(price);
+
+const transaction = (type, { name, price }) => {
+  console.log(type, name, price);
+};
+
+transaction("fucker", products);
