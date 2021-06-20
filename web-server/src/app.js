400474713c068d8c60d81bc88bce541583cfad96
pandula <pandula95@gmail.com>
2021-06-20 17:40:11 +0530

    feat: Serving Up Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,23 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-
 const PORT = 3000;
+
+const publicDirPath = path.join(__dirname, "../public");
+app.use(express.static(publicDirPath));
 
 app.get("", (req, res) => {
   res.send("hello pako");
 });
 
-app.get("/help", (req, res) => {
-  res.send("help hello pako");
-});
+// app.get("/help", (req, res) => {
+//   res.send("help hello pako");
+// });
 
-app.get("/about", (req, res) => {
-  res.send("<h1>Hello Pako 2<h2>");
-});
+// app.get("/about", (req, res) => {
+//   res.send("<h1>Hello Pako 2<h2>");
+// });
 
 app.get("/weather", (req, res) => {
   res.send([
