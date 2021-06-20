663c3c09e839bd28c60ce7373f0645fc2f4bcef0
pandula <pandula95@gmail.com>
2021-06-20 15:22:48 +0530

    style: Set up HTML and JSON outputs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -12,6 +12,19 @@ app.get("/help", (req, res) => {
   res.send("help hello pako");
 });
 
+app.get("/about", (req, res) => {
+  res.send("<h1>Hello Pako 2<h2>");
+});
+
+app.get("/weather", (req, res) => {
+  res.send([
+    {
+      forecast: "Esketit",
+      Location: "SL",
+    },
+  ]);
+});
+
 app.listen(PORT, () => {
   console.log(`Port ` + PORT + ` is UP`);
 });
