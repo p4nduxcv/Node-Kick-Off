3948eabf407757b191a068fa710e73b0fd5a4fbe
pandula <pandula95@gmail.com>
2021-07-02 23:08:01 +0530

    feat: :sparkles: Wiring Up the user Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,6 +2,11 @@ console.log("Client side javascript file is loaded!");
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const messageOne = document.querySelector("#mid1");
+const messageTwo = document.querySelector("#mid2");
+
+messageOne.textContent = "";
+messageTwo.textContent = "Processing....";
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -13,9 +18,12 @@ weatherForm.addEventListener("submit", (e) => {
       response.json().then((data) => {
         if (data.error) {
           console.log(`error`, data.error);
+          messageTwo.textContent = data.error;
         } else {
           console.log(data.location);
           console.log(data.forecast);
+          messageTwo.textContent =
+            "Location :" + data.location + " ForeCast :" + data.forecast;
         }
       });
     }
