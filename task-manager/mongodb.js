ea4826879d3a2b6cc0214bb9946a842c5eed0d46
pandula <pandula95@gmail.com>
2021-07-05 23:23:59 +0530

    feat: :sparkles: add collection

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -16,5 +16,11 @@ MongoClient.connect(
       return console.log("Unable to connet DB"); // when we are returning this it will stop executing from this part
     }
     console.log("Connected Correctly");
+
+    const DB = client.db(databaseName);
+    DB.collection("users").insertOne({
+      name: "Pandu",
+      age: 27,
+    });
   }
 );
