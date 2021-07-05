31707ef231b474f460eed195f7beeb03b98e3bee
pandula <pandula95@gmail.com>
2021-07-05 23:17:38 +0530

    feat: :art: Initiate connection between Mongo and Node JS
    before do that I installed mongoDb communitor MSI and Compass. That will be every easy

---
+++ task-manager/mongodb.js
@@ -0,0 +1,20 @@
+/*
+ ** C R U D
+ */
+
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connet DB"); // when we are returning this it will stop executing from this part
+    }
+    console.log("Connected Correctly");
+  }
+);
