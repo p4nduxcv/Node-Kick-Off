6d3077c89a915547786551863bcd594caf644ac7
pandula <pandula95@gmail.com>
2021-07-10 22:29:15 +0530

    validation and sanitization p1

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const db = "mongodb://127.0.0.1:27017/task-manager-api";
 
@@ -18,51 +19,60 @@ mongoose.connect(
   }
 );
 
-// // set up models
-// const User = mongoose.model("User", {
-//   name: {
-//     type: String,
-//   },
-//   age: {
-//     type: Number,
-//   },
-// });
-
-// // create data
-// const me = new User({
-//   name: "Andrew",
-//   age: 27,
-// });
-
-// // save data in mongo
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch((e) => {
-//     console.log(`error`, e);
-//   });
-
-// set up task model
-const Task = mongoose.model("Task", {
-  description: {
+// set up models
+const User = mongoose.model("User", {
+  name: {
     type: String,
   },
-  completed: {
-    type: Boolean,
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email ඔනේ පකෝ");
+      }
+    },
+  },
+  age: {
+    type: Number,
   },
 });
 
 // create data
-const mytask = new Task({
-  description: "pako",
-  completed: true,
+const me = new User({
+  name: "Andrew",
+  email: "x",
+  age: 27,
 });
 
-mytask
-  .save()
+// save data in mongo
+me.save()
   .then(() => {
-    console.log(mytask);
+    console.log(me);
+  })
+  .catch((e) => {
+    console.log(`error`, e);
+  });
+
+// set up task model
+// const Task = mongoose.model("Task", {
+//   description: {
+//     type: String,
+//   },
+//   completed: {
+//     type: Boolean,
+//   },
+// });
+
+// create data
+// const mytask = new Task({
+//   description: "pako",
+//   completed: true,
+// });
+
+me.save()
+  .then(() => {
+    console.log(me);
   })
   .catch((e) => {
     console.log(e);
