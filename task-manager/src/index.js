3c54a175012a149fe7d1c547a552418d8dd56b08
pandula <pandula95@gmail.com>
2021-07-11 13:53:52 +0530

    set endpoint p2

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("./models/user");
+const Task = require("./models/task");
 require("./db/mongoose");
 
 const app = express();
@@ -17,6 +18,45 @@ app.post("/users", (req, res) => {
     })
     .catch((e) => {
       res.status(400).send(e);
+    });
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(500).send();
     });
 });
 
