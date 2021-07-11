48be4bec0a2722471d94cfe6f5af1a00defa801b
pandula <pandula95@gmail.com>
2021-07-11 23:47:56 +0530

    migrate to async await

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,20 +8,27 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(() => {
-      res.send(user);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+  try {
+    await user.save();
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
+
+  // user
+  //   .save()
+  //   .then(() => {
+  //     res.send(user);
+  //   })
+  //   .catch((e) => {
+  //     res.status(400).send(e);
+  //   });
 });
 
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
   task
