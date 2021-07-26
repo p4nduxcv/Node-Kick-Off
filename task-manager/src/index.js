98da9f66553c9dc297d5e1231f72fa7da927cb43
pandula <pandula95@gmail.com>
2021-07-26 23:20:01 +0530

    feat: :lock: security storing passwords : part 1

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,3 +1,5 @@
+// encriptions
+const bcrypt = require("bcryptjs");
 const express = require("express");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
@@ -13,3 +15,17 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
+
+const encryptPassword = async () => {
+  const password = "password";
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  console.log(`password `, password, ` encrypted Password `, hashedPassword);
+
+  //check password and encrypted password
+  const isMatch = await bcrypt.compare(password, hashedPassword);
+
+  console.log(isMatch);
+};
+
+encryptPassword();
