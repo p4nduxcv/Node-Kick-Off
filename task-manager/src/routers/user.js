5a335bb0cb8f7f2fd2f6e79253fd253710d07a05
pandula <pandula95@gmail.com>
2021-08-09 08:19:15 +0530

    feat: :sparkles: generate authentication tokens

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -7,7 +7,8 @@ router.post("/users", async (req, res) => {
 
   try {
     await user.save();
-    res.status(200).send(user);
+    const token = await user.generateAuthToken();
+    res.status(200).send({ user, token });
   } catch (error) {
     res.status(500).send();
   }
@@ -28,8 +29,10 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    res.send(user);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (e) {
+    console.log(e);
     res.status(400).send();
   }
 });
